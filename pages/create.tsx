f9e048e4590378712b84f48b4d8b5307628705b4
abias <abias1122@gmail.com>
2022-04-22 08:00:03 -0400

    Add blank create charcter page

---
+++ pages/create.tsx
@@ -0,0 +1,6 @@
+import CreateCharacter from "../src/views/create/CreateCharacter";
+import { NextPage } from "next";
+
+const CreateCharacterPage: NextPage = () => <CreateCharacter />;
+
+export default CreateCharacterPage;
---
+++ src/views/create/CreateCharacter.tsx
@@ -0,0 +1,13 @@
+import classes from "./CreateCharacter.module.css";
+
+const CreateCharacter = (): JSX.Element => {
+  return (
+    <>
+      <main className={classes.main}>
+        <div className={classes.content}></div>
+      </main>
+    </>
+  );
+};
+
+export default CreateCharacter;
