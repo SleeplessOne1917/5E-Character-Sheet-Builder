0204de4d6a5a5bc68437ee0753896afe399ffb92
abias <abias1122@gmail.com>
2022-04-21 22:22:41 -0400

    Remove tailwind from home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,20 +1,19 @@
 import Link from "next/link";
+import classes from "./Home.module.css";
 
 const Home = (): JSX.Element => {
   return (
-    <main className="flex justify-center items-center grow">
-      <div className="flex flex-col justify-center items-center z-10">
-        <p className="font-display text-2xl text-black bg-heavens-down-semi-trans rounded-xl p-2 mb-3">
+    <main className={classes.main}>
+      <div className={classes.content}>
+        <p className={classes.blurb}>
           Create awesome{" "}
-          <span className="text-red-wine-800 font-bold">
-            D&D 5<span className="align-super text-sm">th</span> Edition
+          <span className={classes.dnd}>
+            D&D 5<span className={classes.supertext}>th</span> Edition
           </span>{" "}
           character sheets with this free and open-source online tool.
         </p>
         <Link href="/create">
-          <a className="font-fantasy bg-red-wine-800 w-fit p-3 text-heavens-down text-2xl rounded-xl hover:bg-red-wine-700 active:bg-red-wine-600 active:translate-y-1">
-            Get Started
-          </a>
+          <a className={classes["cta-link"]}>Get Started</a>
         </Link>
       </div>
     </main>
