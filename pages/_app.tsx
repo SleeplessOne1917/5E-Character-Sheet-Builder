69a6f1fc15d530f3a8663f2f3b7c448647b1297b
abias <abias1122@gmail.com>
2022-04-24 07:04:29 -0400

    Add footer with link to source code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Footer from '../src/components/Footer/Footer';
 import Header from '../src/components/Header/Header';
 import Image from 'next/image';
 import SectionBar from '../src/components/character-creation/SectionBar/SectionBar';
@@ -21,6 +22,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 			<Header />
 			{pathname.includes('create') && <SectionBar />}
 			<Component {...pageProps} />
+			<Footer />
 		</>
 	);
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import classes from './Footer.module.css';
+
+const Footer = (): JSX.Element => (
+	<footer className={classes.footer}>
+		<ul className={classes['link-list']}>
+			<li>
+				<a
+					href="https://github.com/abias1122/5E-Character-Sheet-Builder"
+					className={classes.link}
+				>
+					<svg className={classes['github-icon']}>
+						<use xlinkHref="/Icons.svg#github" />
+					</svg>
+					Check out the source code!
+				</a>
+			</li>
+		</ul>
+	</footer>
+);
+
+export default Footer;
