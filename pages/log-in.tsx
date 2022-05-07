c4fe4814d326e2f8471ffcb30c8e82bb0be3b9f2
abias <abias1122@gmail.com>
2022-05-06 21:49:04 -0400

    Redirect away from log in and sign up when signed in

--- pages/log-in.tsx
+++ pages/log-in.tsx
@@ -1,6 +1,20 @@
 import LogInView from '../src/views/LogInSignUp/LogIn/LogIn';
 import { NextPage } from 'next';
+import { useAppSelector } from '../src/hooks/reduxHooks';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-const LogInPage: NextPage = () => <LogInView />;
+const LogInPage: NextPage = () => {
+	const router = useRouter();
+	const viewer = useAppSelector(state => state.viewer);
+
+	useEffect(() => {
+		if (viewer) {
+			router.replace('/');
+		}
+	}, [router, viewer]);
+
+	return <LogInView />;
+};
 
 export default LogInPage;
--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,6 +1,20 @@
 import { NextPage } from 'next';
 import SignUpView from '../src/views/LogInSignUp/SignUp/SignUp';
+import { useAppSelector } from '../src/hooks/reduxHooks';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-const SignUpPage: NextPage = () => <SignUpView />;
+const SignUpPage: NextPage = () => {
+	const router = useRouter();
+	const viewer = useAppSelector(state => state.viewer);
+
+	useEffect(() => {
+		if (viewer) {
+			router.replace('/');
+		}
+	}, [router, viewer]);
+
+	return <SignUpView />;
+};
 
 export default SignUpPage;
