499b9bd1368a57daa7948fa18fcc9da6f25997b8
abias <abias1122@gmail.com>
2022-05-07 06:20:45 -0400

    Add generation methods select

--- src/views/create/abilities/Abilities.tsx
+++ src/views/create/abilities/Abilities.tsx
@@ -1,6 +1,8 @@
+import { ChangeEventHandler, useCallback, useState } from 'react';
+
 import AbilityCalculation from '../../../components/character-creation/Abilities/AbilityCalculation';
 import { AbilityItem } from '../../../types/srd';
-import abilitiesClasses from './Abilities.module.css';
+import classes from './Abilities.module.css';
 import commonClasses from '../../Views.module.css';
 
 type AbilitiesProps = {
@@ -8,10 +10,36 @@ type AbilitiesProps = {
 };
 
 const Abilities = ({ abilities }: AbilitiesProps): JSX.Element => {
+	const [generationMethod, setGenerationMethod] = useState('roll');
+
+	const handleGenerationMethodChange: ChangeEventHandler<HTMLSelectElement> =
+		useCallback(
+			event => {
+				setGenerationMethod(event.target.value);
+			},
+			[setGenerationMethod]
+		);
+
 	return (
 		<main className={commonClasses.main}>
 			<div className={commonClasses.content}>
-				<div className={abilitiesClasses['calculations-container']}>
+				<h1 className={classes.title}>Ability Scores</h1>
+				<div className={classes['generation-control']}>
+					<label htmlFor="generation-methods">Generation Method</label>
+					<select
+						id="generation-methods"
+						value={generationMethod}
+						onChange={handleGenerationMethodChange}
+					>
+						<option value="roll">Manual/Rolled</option>
+						<option value="point-buy">Point Buy</option>
+						<option value="array">Standard Array</option>
+					</select>
+				</div>
+				{generationMethod === 'roll' && 'Roll'}
+				{generationMethod === 'point-buy' && 'Point Buy'}
+				{generationMethod === 'array' && 'Standard Array'}
+				<div className={classes['calculations-container']}>
 					{abilities.map(ability => (
 						<AbilityCalculation
 							key={ability.index}
