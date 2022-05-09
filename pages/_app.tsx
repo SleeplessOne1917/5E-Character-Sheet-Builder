bb2765f711ef725847253e9d9413975501eae504
abias <abias1122@gmail.com>
2022-05-09 05:16:46 -0400

    Tweak favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -71,7 +71,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta httpEquiv="ScreenOrientation" content="autoRotate:disabled" />
-				<link rel="icon" type="image/ico" href="/favicon.ico" />
+				<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
 			</Head>
 			<Image
 				src="/images/character-sheet-with-dice.jpg"
