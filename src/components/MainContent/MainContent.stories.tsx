14bba401ae3516c0e561eaf561d5d3322d90cc2b
abias <abias1122@gmail.com>
2022-05-14 08:33:06 -0400

    Add storybook for main content

---
+++ src/components/MainContent/MainContent.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import MainContent from './MainContent';
+
+const MainContentSb = {
+	title: 'Components/MainContent',
+	component: MainContent
+} as ComponentMeta<typeof MainContent>;
+
+export default MainContentSb;
+
+export const Content: ComponentStory<typeof MainContent> = () => (
+	<MainContent>Content</MainContent>
+);
+Content.decorators = [
+	story => (
+		<div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+			{story()}
+		</div>
+	)
+];
