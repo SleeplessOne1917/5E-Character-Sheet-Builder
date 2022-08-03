224ff04e5df029a8685d04abc9e572053220978a
abias <abias@emoneyadvisor.com>
2022-08-03 15:01:43 -0400

    Fix babel issue

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+	dir: './'
+});
+
+const customJestConfig = {
 	collectCoverageFrom: [
 		'**/*.{js,jsx,ts,tsx}',
 		'!**/*.d.ts',
@@ -22,11 +29,7 @@ module.exports = {
 	// Add more setup options before each test is run
 	// setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 	testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-	testEnvironment: 'jsdom',
-	transform: {
-		// Use babel-jest to transpile tests with the next/babel preset
-		// https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-		'^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-	},
-	transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$']
+	testEnvironment: 'jest-environment-jsdom'
 };
+
+module.exports = createJestConfig(customJestConfig);
