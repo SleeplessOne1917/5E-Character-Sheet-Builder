be31f75dbd2ae5da95f0365622f879e9d52978d9
abias <abias@emoneyadvisor.com>
2022-08-03 15:11:36 -0400

    Fix Account view story

--- src/views/Account/Account.stories.tsx
+++ src/views/Account/Account.stories.tsx
@@ -1,10 +1,23 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import Account from './Account';
+import { Provider } from 'react-redux';
+import { getTestStore } from '../../redux/store';
+
+const MockStore = ({ children }: { children: ReactNode }) => (
+	<Provider store={getTestStore()}>{children}</Provider>
+);
 
 export default {
 	title: 'Views/Account',
-	component: Account
+	component: Account,
+	decorators: [
+		Story => (
+			<MockStore>
+				<Story />
+			</MockStore>
+		)
+	]
 } as ComponentMeta<typeof Account>;
 
 const Template: ComponentStory<typeof Account> = () => <Account />;
