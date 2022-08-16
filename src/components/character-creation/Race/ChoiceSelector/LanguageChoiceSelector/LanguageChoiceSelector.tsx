74478de8ba0816065654e651c34899ed00c4f51e
abias <abias@emoneyadvisor.com>
2022-08-16 14:19:09 -0400

    Use custom select for language choice selector

--- src/components/character-creation/Race/ChoiceSelector/LanguageChoiceSelector/LanguageChoiceSelector.tsx
+++ src/components/character-creation/Race/ChoiceSelector/LanguageChoiceSelector/LanguageChoiceSelector.tsx
@@ -1,8 +1,8 @@
-import { ChangeEvent, useCallback, useState } from 'react';
 import { SrdItem, SrdItemChoice } from '../../../../../types/srd';
+import { useCallback, useState } from 'react';
 
 import ChoiceSelector from '../ChoiceSelector';
-import classes from '../ChoiceSelector.module.css';
+import Select from '../../../../Select/Select';
 import { useAppSelector } from '../../../../../hooks/reduxHooks';
 
 type OptionSelectorProps = {
@@ -37,9 +37,9 @@ const LanguageChoiceSelector = ({
 	const languages = useAppSelector(state => state.editingCharacter.languages);
 
 	const handleChangeSelect = useCallback(
-		(index: number, event: ChangeEvent<HTMLSelectElement>) =>
+		(index: number, selectValue: string) =>
 			setSelectValues(prevState =>
-				prevState.map((value, i) => (i === index ? event.target.value : value))
+				prevState.map((value, i) => (i === index ? selectValue : value))
 			),
 		[setSelectValues]
 	);
@@ -67,30 +67,27 @@ const LanguageChoiceSelector = ({
 
 	for (let i = 0; i < choice.choose; ++i) {
 		selects.push(
-			<select
-				value={selectValues[i]}
-				aria-label="Select choice"
-				onChange={event => handleChangeSelect(i, event)}
-				className={classes.select}
+			<Select
 				key={i}
-			>
-				<option value="blank">&mdash;</option>
-				{choice.from.options
-					.filter(
-						option =>
-							!(
-								selectValues.includes(option.item.index) ||
-								languages
-									.map(language => language.index)
-									.includes(option.item.index)
-							) || option.item.index === selectValues[i]
-					)
-					.map(option => (
-						<option value={option.item.index} key={option.item.index}>
-							{option.item.name}
-						</option>
-					))}
-			</select>
+				value={selectValues[i]}
+				onChange={value => handleChangeSelect(i, value as string)}
+				options={[{ value: 'blank', label: '\u2014' }].concat(
+					choice.from.options
+						.filter(
+							option =>
+								!(
+									selectValues.includes(option.item.index) ||
+									languages
+										.map(language => language.index)
+										.includes(option.item.index)
+								) || option.item.index === selectValues[i]
+						)
+						.map(option => ({
+							value: option.item.index,
+							label: option.item.name
+						}))
+				)}
+			/>
 		);
 	}
 
