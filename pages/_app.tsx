7a3e02f20f7dce962e4a9d93980ee2a2c7c445dd
Anthony Bias <abias1122@gmail.com>
2022-08-18 22:27:12 -0400

    Update favicon to make sure it works

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -71,7 +71,27 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta httpEquiv="ScreenOrientation" content="autoRotate:disabled" />
-				<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+				<meta name="msapplication-TileColor" content="#f7ce65" />
+				<meta name="theme-color" content="#ffffff" />
 			</Head>
 			<Image
 				src="/images/character-sheet-with-dice.jpg"
