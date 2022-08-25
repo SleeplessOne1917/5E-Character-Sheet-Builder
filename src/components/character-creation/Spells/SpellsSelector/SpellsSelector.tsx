9699b4de8c010712ed6acd945f57d39be4bfcecf
abias <abias@emoneyadvisor.com>
2022-08-25 11:52:12 -0400

    Make SpellsSelector filterable

--- src/components/character-creation/Spells/SpellsSelector/SpellsSelector.tsx
+++ src/components/character-creation/Spells/SpellsSelector/SpellsSelector.tsx
@@ -1,14 +1,22 @@
-import { useCallback, useMemo, useState, useEffect } from 'react';
-
-import SpellSelector from '../SpellSelector/SpellSelector';
-import { SrdSpellItem } from '../../../../types/srd';
-import styles from './SpellsSelector.module.css';
-import { useAppDispatch, useAppSelector } from '../../../../hooks/reduxHooks';
+import {
+	ChangeEventHandler,
+	useCallback,
+	useEffect,
+	useMemo,
+	useState
+} from 'react';
+import { SrdItem, SrdSpellItem } from '../../../../types/srd';
 import {
 	addClassSpell,
 	removeClassSpell
 } from '../../../../redux/features/classInfo';
 import { addSpell, removeSpell } from '../../../../redux/features/spellcasting';
+import { useAppDispatch, useAppSelector } from '../../../../hooks/reduxHooks';
+
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+import Select from '../../../Select/Select';
+import SpellSelector from '../SpellSelector/SpellSelector';
+import styles from './SpellsSelector.module.css';
 
 type SpellsSelectorProps = {
 	spells: SrdSpellItem[];
@@ -19,6 +27,39 @@ const SpellsSelector = ({ spells, choose }: SpellsSelectorProps) => {
 	const classSpells = useAppSelector(
 		state => state.editingCharacter.classInfo.spells
 	);
+	const highestSlotLevel = useAppSelector(
+		state => state.editingCharacter.spellcasting.highestSlotLevel
+	);
+	const levels = useMemo(() => {
+		const levels: number[] = [];
+		if (
+			!spells.reduce<{ level: number | null; isSameLevel: boolean }>(
+				(acc, cur) => {
+					if (!acc.level) {
+						return {
+							...acc,
+							level: cur.level
+						};
+					} else {
+						return {
+							...acc,
+							isSameLevel: cur.level === acc.level
+						};
+					}
+				},
+				{
+					level: null,
+					isSameLevel: true
+				}
+			).isSameLevel
+		) {
+			for (let i = 1; i <= highestSlotLevel; ++i) {
+				levels.push(i);
+			}
+		}
+
+		return levels;
+	}, [highestSlotLevel, spells]);
 
 	const dispatch = useAppDispatch();
 
@@ -30,6 +71,10 @@ const SpellsSelector = ({ spells, choose }: SpellsSelectorProps) => {
 		() => Object.values(traitSpellsByTrait).flatMap(value => value),
 		[traitSpellsByTrait]
 	);
+
+	const [search, setSearch] = useState('');
+	const [selectedSchool, setSelectedSchool] = useState('blank');
+	const [selectedLevel, setSelectedLevel] = useState<string | number>('blank');
 
 	const getInitialSelectedSpells = useCallback(() => {
 		const selected = spells
@@ -69,15 +114,107 @@ const SpellsSelector = ({ spells, choose }: SpellsSelectorProps) => {
 		[dispatch]
 	);
 
+	const selectorLabel = `Search ${
+		levels.length === 0 && spells[0].level === 0 ? 'cantrips' : 'spells'
+	}`;
+
+	const handleSearchChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+		event => {
+			setSearch(event.target.value);
+		},
+		[setSearch]
+	);
+
+	const handleSelectedSchoolChange = useCallback(
+		(value: string | number) => {
+			setSelectedSchool(value as string);
+		},
+		[setSelectedSchool]
+	);
+
+	const handleSelectedLevelChange = useCallback(
+		(value: string | number) => {
+			setSelectedLevel(value);
+		},
+		[setSelectedLevel]
+	);
+
 	return (
 		<div data-testid="spells-selector" className={styles.container}>
 			<div className={styles.text}>
 				{selectedSpells.filter(spell => spell !== 'blank').length}/{choose}{' '}
 				{spells.some(({ level }) => level > 0) ? 'spell' : 'cantrip'}s selected
 			</div>
+			<div className={styles['filter-bar']}>
+				<div className={styles['search-container']}>
+					<input
+						className={styles.search}
+						aria-label={selectorLabel}
+						placeholder={selectorLabel}
+						onChange={handleSearchChange}
+						value={search}
+					/>
+					<MagnifyingGlassIcon className={styles['search-icon']} />
+				</div>
+				<div className={styles.selects}>
+					<div className={styles['select-container']}>
+						<label id="school-select" className={styles['select-label']}>
+							School
+						</label>{' '}
+						<Select
+							labelledBy="school-select"
+							options={[{ value: 'blank', label: '\u2014' }].concat(
+								spells
+									.reduce<SrdItem[]>((acc, cur) => {
+										if (
+											!acc.some(school => school.index === cur.school.index)
+										) {
+											return [...acc, cur.school];
+										} else {
+											return acc;
+										}
+									}, [])
+									.map(school => ({ value: school.index, label: school.name }))
+							)}
+							value={selectedSchool}
+							onChange={handleSelectedSchoolChange}
+						/>
+					</div>
+					{levels.length > 0 && (
+						<div className={styles['select-container']}>
+							<label id="level-select" className={styles['select-label']}>
+								Level
+							</label>{' '}
+							<Select
+								labelledBy="level-select"
+								options={[
+									{ value: 'blank', label: '\u2014' } as {
+										value: string | number;
+										label: string;
+									}
+								].concat(
+									levels.map(level => ({
+										value: level,
+										label: `${level}`
+									}))
+								)}
+								value={selectedLevel}
+								onChange={handleSelectedLevelChange}
+							/>
+						</div>
+					)}
+				</div>
+			</div>
 			<div className={styles['spells-container']}>
 				{spells
-					.filter(spell => !traitSpells.some(ts => ts.index === spell.index))
+					.filter(
+						spell =>
+							!traitSpells.some(ts => ts.index === spell.index) &&
+							spell.name.toLowerCase().includes(search.toLowerCase()) &&
+							(selectedSchool === 'blank' ||
+								selectedSchool === spell.school.index) &&
+							(selectedLevel === 'blank' || selectedLevel === spell.level)
+					)
 					.map(spell => (
 						<SpellSelector
 							key={spell.index}
