68dd99a5e3a3057f7a3b94803576b82f1395cb94
Anthony Bias <abias1122@gmail.com>
2022-09-06 20:00:11 -0400

    Add ellipses interval to LoadingPageContent

--- src/components/LoadingPageContent/LoadingPageContent.tsx
+++ src/components/LoadingPageContent/LoadingPageContent.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import MainContent from '../MainContent/MainContent';
 
 const LoadingPageContent = () => {
+	const [dots, setDots] = useState('');
+
+	useEffect(() => {
+		const dotsInterval = setInterval(() => {
+			setDots(prev => {
+				if (prev.length === 3) {
+					return '';
+				} else {
+					return prev + '.';
+				}
+			});
+		}, 1000);
+
+		return () => {
+			clearInterval(dotsInterval);
+		};
+	}, []);
+
 	return (
 		<MainContent testId="loading-page-content">
-			<h1>Loading...</h1>
+			<h1>Loading{dots}</h1>
 			<div
 				style={{
 					display: 'flex',
