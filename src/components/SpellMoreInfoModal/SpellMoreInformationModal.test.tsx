97b585ae8fd0df39fe14589481abbfdb327281cd
SleeplessOne1917 <abias1122@gmail.com>
2022-11-16 20:38:33 -0500

    chore: update dependencies

--- src/components/SpellMoreInfoModal/SpellMoreInformationModal.test.tsx
+++ src/components/SpellMoreInfoModal/SpellMoreInformationModal.test.tsx
@@ -1,10 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import * as Router from 'next/router';
 import * as stories from './SpellMoreInformationModal.stories';
+
+import { render, screen } from '@testing-library/react';
+
 import { composeStories } from '@storybook/testing-react';
 
 const { Default } = composeStories(stories);
 
+const mockRouter = (pathname: string) => {
+	const useRouter = jest.spyOn(Router, 'useRouter');
+
+	const router: Router.NextRouter = {
+		push: () => Promise.resolve(true),
+		prefetch: () => Promise.resolve(),
+		pathname,
+		route: '',
+		query: {},
+		asPath: '',
+		basePath: '',
+		isLocaleDomain: true,
+		reload: jest.fn(),
+		replace: jest.fn(),
+		back: jest.fn(),
+		beforePopState: jest.fn(),
+		events: { emit: jest.fn(), on: jest.fn(), off: jest.fn() },
+		isFallback: false,
+		isPreview: false,
+		isReady: true,
+		forward: () => {}
+	};
+	useRouter.mockReturnValue(router);
+};
+
 it('renders correctly', () => {
+	mockRouter('/create/character/spells');
 	render(<Default />);
 
 	expect(screen.getByTestId('spell-more-information-modal')).toMatchSnapshot();
